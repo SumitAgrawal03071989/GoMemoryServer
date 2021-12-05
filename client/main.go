2b731d7b9c8c90f9004b1f8f9baa82e99507a3cf
Sumit Agrawal <sumitagrawal.03071989@gmail.com>
2021-12-05 15:13:34 +0530

    GoMemoryServer

---
+++ client/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	pb "github.com/sumitagrawal.0309189/simpleproject/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:50052"
+	defaultName = "1"
+)
+
+func main() {
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewMemoryServerClient(conn)
+
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// r, err := c.DoIRemember(ctx, &pb.NumberRequest{Number: name})
+	r, err := c.CheckWithMemoryServer(ctx, &pb.NumberRequest{Number: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
---
+++ server/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/sumitagrawal.0309189/simpleproject/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50052"
+)
+
+type server struct {
+	pb.UnimplementedMemoryServerServer
+}
+
+func itemExists(item string, arr []string) bool {
+	var memoryCapacity = 3
+	for i := len(arr); i > 0 && memoryCapacity > 0; i-- {
+		if arr[i-1] == item {
+			return true
+		}
+		memoryCapacity--
+	}
+	return false
+}
+
+var memory = make([]string, 0)
+
+func (s *server) CheckWithMemoryServer(ctx context.Context, in *pb.NumberRequest) (*pb.DoIRemember, error) {
+
+	for i := 0; i < len(memory); i++ {
+		log.Printf("server_memory %v", memory[i])
+	}
+
+	incomingNumber := in.GetNumber()
+
+	log.Printf("Received: %v", incomingNumber)
+
+	if itemExists(incomingNumber, memory) {
+		return &pb.DoIRemember{Message: "Yes I remember " + incomingNumber}, nil
+	}
+	memory = append(memory, incomingNumber)
+	return &pb.DoIRemember{Message: "It might be that you are telling me this number first time or probably I am getting old its hard for me to remember more than 3 number, I will try to remember " + incomingNumber}, nil
+
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterMemoryServerServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
